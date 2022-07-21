f093229d2313b1f7e9d26b8078402702609cadc4
Masykuri Syaifullah <msyaifullah@gmail.com>
2022-07-21 10:40:53 +0700

    add ssr script

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path')
+const webpack = require('webpack')
+const nodeExternals = require('webpack-node-externals')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const browserConfig = {
+  mode: "production",
+  entry: './src/browser/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env','@babel/preset-react'] }},
+      { test: /\.css$/, use: [ 'css-loader' ]}
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __isBrowser__: "true"
+    })
+  ]
+}
+
+const serverConfig = {
+  mode: "production",
+  entry: './src/server/index.js',
+  target: 'node',
+  externals: [nodeExternals()],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'server.js'
+  },
+  optimization: {
+    minimize: false
+  },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env','@babel/preset-react'] }},
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new webpack.DefinePlugin({
+      __isBrowser__: "false"
+    }),
+  ]
+}
+
+module.exports = [browserConfig, serverConfig]
